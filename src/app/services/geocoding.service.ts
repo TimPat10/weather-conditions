3452cb00b41fc44ccb7f2062aac09db457917fa1
Tim Patton <TimPat10@gmail.com>
2022-07-04 09:50:25 -0400

    better variable names for fake data

--- src/app/services/geocoding.service.ts
+++ src/app/services/geocoding.service.ts
@@ -10,25 +10,28 @@ export class GeocodingService {
 
   constructor() { }
 
+  private _tippCityCoordinates: ICoordinates = {
+    latitude: "39.88963102486146",
+    longitude: "-84.10662579008196"    
+  }
+
+  private _stLouisCoordinates: ICoordinates = {
+    latitude: "38.625490353618716",
+    longitude: "-90.18678253546466"
+  }
+
   public getLatLongFromCityState(city: string, state: string): Observable<ICoordinates> {
     let coordinates: ICoordinates;
 
     if (city.toLocaleLowerCase() == "tipp city" && state.toLocaleLowerCase() == "oh") {
-      coordinates = {
-        latitude: "39.88963102486146",
-        longitude: "-84.10662579008196"
-      }
-      return of(coordinates);
+      return of(this._tippCityCoordinates);
     }
 
     if (city.toLocaleLowerCase() == "st. louis" && state.toLocaleLowerCase() == "mo") {
-      coordinates = {
-        latitude: "38.625490353618716",
-        longitude: "-90.18678253546466"
-      }
-      return of(coordinates);
+      return of(this._stLouisCoordinates);
     }
 
+    // Bad request
     return of({
       latitude: "XXXXX",
       longitude: "YYYYY"
@@ -36,10 +39,18 @@ export class GeocodingService {
   }
 
   public getLatLongFromZipcode(zipcode: string): Observable<ICoordinates> {
-    return of(
-      {
-        latitude: "39.88963102486146",
-        longitude: "-84.10662579008196"
-      });
+    if (zipcode == "45371") {
+      return of(this._tippCityCoordinates);
+    }
+
+    if (zipcode == "63101") {
+      return of(this._stLouisCoordinates);
+    }
+    
+    // Bad request
+    return of({
+      latitude: "XXXXX",
+      longitude: "YYYYY"
+    });
   }
 }
