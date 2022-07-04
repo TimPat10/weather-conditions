4a98e4faa1175e41c3727efc608226e98f5128d6
Tim Patton <TimPat10@gmail.com>
2022-07-03 20:22:29 -0400

    Implemented API Service and its tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { zip } from 'rxjs';
 
 import { ApiService } from './api.service';
 
@@ -10,7 +11,41 @@ describe('ApiService', () => {
     service = TestBed.inject(ApiService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return a Weather Forecast API', () => {
+    const openWeatherMapBaseApi: string = "https://openweathermap.org/data/2.5/weather";
+    const openWeatherMapKey: string  = "cf002751564a4c78f5f7ed479f1b9ba3";
+    const latitude = "39.88963102486146";
+    const longitude = "-84.10662579008196";
+    const params = `lat=${latitude}&lon=${longitude}&apiid=${openWeatherMapKey}`;
+    const expectedApi = `${openWeatherMapBaseApi}?${params}`.toLowerCase();
+    const actualApi = service.getWeatherForecastApi(latitude, longitude).toLowerCase();
+
+    expect(actualApi).toBe(expectedApi);
+  });
+
+  it('should return a Latitude/Longitude API from City and State', () => {
+    const latLongBaseApi: string = "https://someLatitudeLongitudeApi";
+    const someLatLongApiKey: string  = "someLatLongApiKey";
+    const city = "Tipp City";
+    const cityParam = encodeURIComponent(city.trim());
+    const state = "OH";
+    const params = `city=${cityParam}&state=${state}&apiid=${someLatLongApiKey}`;
+    const expectedApi = `${latLongBaseApi}?${params}`.toLowerCase();
+    const actualApi = service.getLatLongFromCityStateApi(city, state).toLowerCase();
+
+    expect(actualApi).toBe(expectedApi);
+  });
+
+  it('should return a Latitude/Longitude API from Zip Code', () => {
+    const someLatLongBaseApi: string = "https://someLatitudeLongitudeApi";
+    const someLatLongApiKey: string  = "someLatLongApiKey";
+    const zipcode = "45371";
+    const zipcodeParam = encodeURIComponent(zipcode.trim());
+
+    const params = `zip=${zipcodeParam}&apiid=${someLatLongApiKey}`;
+    const expectedApi = `${someLatLongBaseApi}?${params}`.toLowerCase();
+    const actualApi = service.getLatLongFromZipApi(zipcode).toLowerCase();
+
+    expect(actualApi).toBe(expectedApi);
   });
 });
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
 
+//
+// The API service is responsible for constructing the URLs that will connect to Web APIs
+//
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+  
   constructor() { }
+
+  //#region  APIs to get current weather conditions
+
+  private _openWeatherMapBaseApi: string = "https://openweathermap.org/data/2.5/weather";
+  private _openWeatherMapKey: string  = "cf002751564a4c78f5f7ed479f1b9ba3";
+
+  public getWeatherForecastApi(latitude: string, longitude:string): string {
+    const params = `lat=${latitude}&lon=${longitude}&apiid=${this._openWeatherMapKey}`;
+    const api = `${this._openWeatherMapBaseApi}?${params}`.toLowerCase();
+    return api;
+  }
+
+  //#endregion  APIs to get current weather conditions
+
+  //#region APIs to get latitude and longitude
+
+  private _someLatLongBaseApi = "https://someLatitudeLongitudeApi";
+  private _someLatLongApiKey: string  = "someLatLongApiKey";
+
+  public getLatLongFromCityStateApi(city: string, state: string) {
+    const cityParam = encodeURIComponent(city.trim());
+    const params = `city=${cityParam}&state=${state}&apiid=${this._someLatLongApiKey}`;
+
+    const api = `${this._someLatLongBaseApi}?${params}`.toLowerCase();
+    return api;
+  }
+
+  public getLatLongFromZipApi(zipcode: string) {
+    const zipcodeParam = encodeURIComponent(zipcode.trim());
+    const params = `zip=${zipcodeParam}&apiid=${this._someLatLongApiKey}`;
+
+    const api = `${this._someLatLongBaseApi}?${params}`.toLowerCase();
+    return api;
+  }
+
+  //#endregion APIs to get latitude and longitude
 }
